b96603888cec187c84e10c77daaa05fa093c7975
ZakTan <Ztanksley@luc.edu>
2024-08-16 13:12:16 -0700

    Adjusting locic

--- DatingCalculator/ViewModel/DatingCalculatorVM.swift
+++ DatingCalculator/ViewModel/DatingCalculatorVM.swift
@@ -25,40 +25,63 @@ class DatingCalculatorVM: ObservableObject {
         switch gender {
         case .female:
             switch (hot, crazy) {
+                // No-Go Zone (Hot < 5, Crazy > 7)
             case (let h, let c) where h < 5 && c > 7:
                 videoFileName = "no_go_zone"
                 description =  "no_go_zone"
-            case (let h, let c) where h >= 5 && h < 8 && c > 5 && c <= 7:
+                // Fun Zone (Hot 5-7, Crazy 5-7)
+            case (let h, let c) where (5...7).contains(h) && (5...7).contains(c):
                 videoFileName = "fun_zone"
                 description =  "fun_zone"
-            case (let h, let c) where h >= 8 && c > 7:
+                //Danger Zone (Hot > 7, Crazy > 7)
+            case (let h, let c) where h > 7 && c > 7:
                 videoFileName = "danger_zone"
                 description =  "danger_zone"
-            case (let h, let c) where h >= 6 && h < 8 && c <= 5:
+                // Date Zone (Hot 6-8, Crazy ≤ 5)
+            case (let h, let c) where (6...8).contains(h) && c <= 5:
                 videoFileName = "date_zone"
                 description =  "date_zone"
-            case (let h, let c) where h >= 8 && c <= 5:
+                // Wife Zone (Hot > 8, Crazy ≤ 5)
+            case (let h, let c) where h > 8 && c <= 5:
                 videoFileName = "wife_zone"
-                description =  "Wife Zone"
+                description =  "Marry Zone"
+                //Unicorn Zone (Hot > 8, Crazy < 4)
+            case (let h, let c) where h > 8 && c < 4:
+                videoFileName = "unicorn_F"
+                description =  "Only in the Movies"
             default:
                 videoFileName = "undefined_zone"
                 description =  "undefined_zone"
             }
         case .male:
-            // Different logic for males (this is just an example)
             switch (hot, crazy) {
-            case (let h, let c) where h < 4 && c > 6:
+               // No-Go Zone (Hot < 5, Success < 5
+            case (let h, let c) where h < 5 && c < 5:
                 videoFileName = "no_go_zone"
-            case (let h, let c) where h >= 4 && h < 7 && c > 4 && c <= 6:
+                description =  "no_go_zone"
+                // Fun Zone (Hot 5-7, Success 5-7)
+            case (let h, let c) where (5...7).contains(h) && (5...7).contains(c):
                 videoFileName = "fun_zone"
-            case (let h, let c) where h >= 7 && c > 6:
-                videoFileName = "danger_zone.mp4"
-            case (let h, let c) where h >= 5 && h < 7 && c <= 4:
+                description =  "fun_zone"
+                // Danger Zone (Hot > 7, Success < 5):
+            case (let h, let c) where h > 7 && c < 5:
+                videoFileName = "danger_zone"
+                description =  "danger_zone"
+                // Date Zone (Hot 6-8, Success 6-8)
+            case (let h, let c) where (6...8).contains(h) && (6...8).contains(c):
                 videoFileName = "date_zone"
-            case (let h, let c) where h >= 7 && c <= 4:
-                videoFileName = "wife_zone"
+                description =  "date_zone"
+                // Marriage Zone (Hot > 8, Success > 8)
+            case (let h, let c) where h > 8 && c < 5:
+                videoFileName = "Marry Zone"
+                description =  "Marry Zone"
+                //Unicorn Zone (Hot > 8, Crazy < 4)
+            case (let h, let c) where h > 8 && c < 2:
+                videoFileName = "unicorn_M"
+                description =  "Only in the Movies"
             default:
                 videoFileName = "undefined_zone"
+                description =  "undefined_zone"
             }
         }
     }
